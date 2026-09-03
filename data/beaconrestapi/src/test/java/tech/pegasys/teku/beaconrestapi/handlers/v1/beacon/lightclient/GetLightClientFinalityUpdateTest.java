9fe3cab28f940ac5c1ad9cc59aa112a809cf43ae
Abhivansh <31abhivanshj@gmail.com>
2026-09-04 03:01:49 +0530

    test: add GetLightClientFinalityUpdate tests

---
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/lightclient/GetLightClientFinalityUpdateTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright Consensys Software Inc., 2026
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.lightclient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_ACCEPTABLE;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_FOUND;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
+import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_CONSENSUS_VERSION;
+import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.getResponseStringFromMetadata;
+import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.verifyMetadataErrorResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.io.Resources;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.TestSpecFactory;
+import tech.pegasys.teku.spec.datastructures.lightclient.LightClientFinalityUpdate;
+
+public class GetLightClientFinalityUpdateTest extends AbstractMigratedBeaconHandlerTest {
+
+  @BeforeEach
+  void setup() {
+    // we need altair minimum, so update spec
+    setSpec(TestSpecFactory.createMinimalAltair());
+    setHandler(new GetLightClientFinalityUpdate(schemaDefinitionCache, chainDataProvider));
+  }
+
+  @Test
+  void shouldReturnLightClientFinalityUpdate() throws Exception {
+    final LightClientFinalityUpdate lightClientFinalityUpdate =
+        dataStructureUtil.randomLightClientFinalityUpdate(UInt64.ONE);
+
+    when(chainDataProvider.getLatestLightClientFinalityUpdate())
+        .thenReturn(Optional.of(lightClientFinalityUpdate));
+
+    handler.handleRequest(request);
+
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(request.getResponseBody()).isEqualTo(lightClientFinalityUpdate);
+    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_VERSION))
+        .isEqualTo(SpecMilestone.ALTAIR.lowerCaseName());
+  }
+
+  @Test
+  void shouldReturnNotFoundWhenNoFinalityUpdateAvailable() throws Exception {
+    when(chainDataProvider.getLatestLightClientFinalityUpdate()).thenReturn(Optional.empty());
+
+    handler.handleRequest(request);
+
+    assertThat(request.getResponseCode()).isEqualTo(SC_NOT_FOUND);
+  }
+
+  @Test
+  void metadata_shouldHandle200() throws Exception {
+    final LightClientFinalityUpdate lightClientFinalityUpdate =
+        dataStructureUtil.randomLightClientFinalityUpdate(UInt64.ONE);
+
+    final String data = getResponseStringFromMetadata(handler, SC_OK, lightClientFinalityUpdate);
+    final String expected =
+        Resources.toString(
+            Resources.getResource(
+                GetLightClientFinalityUpdateTest.class, "getLightClientFinalityUpdate.json"),
+            StandardCharsets.UTF_8);
+    assertThat(data).isEqualTo(expected);
+  }
+
+  @Test
+  void metadata_shouldHandle404() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_NOT_FOUND);
+  }
+
+  @Test
+  void metadata_shouldHandle406() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_NOT_ACCEPTABLE);
+  }
+
+  @Test
+  void metadata_shouldHandle500() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
+  }
+}
